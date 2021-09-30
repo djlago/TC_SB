82ae9fad7586930754c037383b544d0b25e68614
Daniel Lagomarsino <daniel.lagomarsino@smartbear.com>
2021-09-30 10:45:43 -0400

    TestComplete
    TC Tests

---
+++ TC_SB/Script/Unit1.py
@@ -0,0 +1,6 @@
+﻿def ReadWholeFile():
+  file = ProjectSuite.Path + "\parameters.txt"  
+  s = aqFile.ReadWholeTextFile(file, aqFile.ctANSI)
+  Log.Message("File entire contents:")
+  Log.Message(s)
+Project.Variables.URL = s
